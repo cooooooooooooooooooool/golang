c0058c19f4814eef72823318d582622a9dda0c1b
이재만(솔루션개발 Big-Y2 그룹) <jaemanii.lee@poscoict.com>
2020-05-26 19:45:38 +0900

    http 와 json example

---
+++ src/poscoict.com/example/json/decoding/json.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+//Member -
+type Member struct {
+	Name   string
+	Age    int
+	Active bool
+}
+
+func main() {
+
+	// 테스트용 JSON 데이타
+	jsonBytes, _ := json.Marshal(Member{"Tim", 1, true})
+
+	// JSON 디코딩
+	var mem Member
+	err := json.Unmarshal(jsonBytes, &mem)
+	if err != nil {
+		panic(err)
+	}
+
+	// mem 구조체 필드 엑세스
+	fmt.Println(mem.Name, mem.Age, mem.Active)
+}
